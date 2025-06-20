d4ff2e7102de4a72a2c5be7d1f0c0fe5145c7bc8
Leandro Huber <leandro.huber@hammerconsult.com.br>
2025-06-20 16:08:43 -0300

    Ajuste

--- main.ts
+++ main.ts
@@ -1,27 +1,33 @@
 import { Connection } from "./modules/Connection.js";
 import { Stormglass } from "./modules/Stormglass.js";
-import dotenv from 'dotenv';
-import cron from 'node-cron';
-dotenv.config()
+import dotenv from "dotenv";
+import cron from "node-cron";
+dotenv.config();
 class main {
     constructor() {
+        const connection = new Connection();
+        const stormglass = new Stormglass();
 
-        const connection = new Connection()
-        const stormglass = new Stormglass()
-        
-        cron.schedule('0 */6 * * *', () => {
-            this.weatherDataUpdate(connection, stormglass);
-        }, {
-            timezone: "America/Sao_Paulo"
-        });
+        cron.schedule(
+            "0 */6 * * *",
+            () => {
+                this.weatherDataUpdate(connection, stormglass);
+            },
+            {
+                timezone: "America/Sao_Paulo",
+            }
+        );
     }
 
-    weatherDataUpdate = async (connection: IConnection, provider: IProvider) => {
-        const partials = await provider.getWeather()
-        partials.forEach(partial => {
-            connection.save(partial)
+    weatherDataUpdate = async (
+        connection: IConnection,
+        provider: IProvider
+    ) => {
+        const partials = await provider.getWeather();
+        partials.forEach((partial) => {
+            connection.save(partial);
         });
-    }
+    };
 }
 
 new main();
